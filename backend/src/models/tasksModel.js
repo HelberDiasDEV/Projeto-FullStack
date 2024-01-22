f2ddc0ed8bc438a8cdf376d5575ed723cc75cb84
Helber Dias <98468468+HelberDiasDEV@users.noreply.github.com>
2024-01-22 19:33:24 -0300

    ADD backend
    router
    server
    controller
    app
    connection
    tasksModel

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+    const tasks = await connection.execute('SELEC * FROM tasks');
+    return tasks;
+};
+
+module.exports = {
+    getAll
+};
