ba55d2675ac18ae8197c6865bf809a281687e8dd
Helber Dias <98468468+HelberDiasDEV@users.noreply.github.com>
2024-02-05 19:59:04 -0300

    add delete, update
    ValidateFieldTitle, ValidateFieldStatus.

--- backend/src/middlewares/tasksMiddlewares.js
+++ backend/src/middlewares/tasksMiddlewares.js
@@ -1,16 +1,32 @@
-const validateBody = (req, res, next) => {
+const { response } = require("../app");
+
+const validateFieldTitle = (req, res, next) => {
 
     const { body } = req;
 
     if(body.title == undefined) {
-       return res.status(400).json({ message: 'O campo titulo é obrigatório.'})
+       return res.status(400).json({ message: 'O campo "titulo" é obrigatório.'})
     }
     if(body.title == undefined) {
-        return res.status(400).json({ message: 'O campo titulo não pode está vazio.'})
+        return res.status(400).json({ message: 'O campo "titulo" não pode está vazio.'})
     }
     next();
 };
 
+const validateFieldStatus = (req, res, next) => {
+    const { body } = req;
+    
+    if (body.status == undefined) {
+        return res.status(400).json({ message: 'O  campo "status" é obrigatório.'})
+    }
+    if (body.status == '') {
+        return res.status(400).json({ message: 'O campo "status" não pode está vazio.'});
+    }
+
+    next();
+};
+
 module.exports = {
-    validateBody,
+    validateFieldTitle ,
+    validateFieldStatus
 }
